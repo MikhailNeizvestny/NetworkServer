8e4516588a40cabff1c1d53d5e0b65fafb99aaa3
MikhailNeizvestny <brainus05@yandex.ru>
2018-03-27 20:12:39 +0400

    Добавьте файлы проекта.

---
+++ NetworkServer/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace NetworkServer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonStartServer = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.buttonCatalog = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonStartServer
+            // 
+            this.buttonStartServer.Location = new System.Drawing.Point(12, 12);
+            this.buttonStartServer.Name = "buttonStartServer";
+            this.buttonStartServer.Size = new System.Drawing.Size(88, 81);
+            this.buttonStartServer.TabIndex = 0;
+            this.buttonStartServer.Text = "Запустить сервер";
+            this.buttonStartServer.UseVisualStyleBackColor = true;
+            this.buttonStartServer.Click += new System.EventHandler(this.buttonStartServer_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(168, 33);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(203, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // buttonCatalog
+            // 
+            this.buttonCatalog.Location = new System.Drawing.Point(168, 61);
+            this.buttonCatalog.Name = "buttonCatalog";
+            this.buttonCatalog.Size = new System.Drawing.Size(85, 32);
+            this.buttonCatalog.TabIndex = 2;
+            this.buttonCatalog.Text = "Выбрать";
+            this.buttonCatalog.UseVisualStyleBackColor = true;
+            this.buttonCatalog.Click += new System.EventHandler(this.buttonCatalog_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(165, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Каталог";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(383, 175);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonCatalog);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.buttonStartServer);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonStartServer;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button buttonCatalog;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ NetworkServer/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NetworkServer
+{
+    public partial class Form1 : Form
+    {
+        private TcpClient client;
+        private TcpListener server;
+        private NetworkStream mainStream;
+        BinaryFormatter binFormater;
+        static Rectangle bounds;
+        static Bitmap screen;
+        private readonly Thread Listening;
+        private string path;
+
+        public Form1()
+        {
+            path = "C:\\Users\\Cyfralus\\Documents\\Visual Studio 2015\\Projects\\WindowsFormsApplication2\\WindowsFormsApplication2\\bin\\Debug\\picture.png";
+            client = new TcpClient();
+            Listening = new Thread(StartListening);
+            binFormater = new BinaryFormatter();
+            InitializeComponent();
+        }
+
+        private void buttonStartServer_Click(object sender, EventArgs e)
+        {
+            server = new TcpListener(IPAddress.Any, 8000);
+            Listening.Start();
+        }
+
+        private void StartListening()
+        {
+            while (true)
+            {
+                while (!client.Connected)
+                {
+                    server.Start();
+                    client = server.AcceptTcpClient();
+                    MessageBox.Show("client");
+                    BinaryFormatter binFormater = new BinaryFormatter();
+                    if (client.Connected)
+                    {
+                        mainStream = client.GetStream();
+                        screen = (Bitmap)binFormater.Deserialize(mainStream);
+                        screen.Save(path, ImageFormat.Png);
+                    }
+                }
+                client.Close();
+            }
+        }
+
+        private void StopListening()
+        {
+            if (server != null)
+            {
+                server.Stop();
+                client = null;
+                if (Listening.IsAlive) Listening.Abort();
+            }
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            base.OnFormClosing(e);
+            StopListening();
+        }
+
+        private void buttonCatalog_Click(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog dialog = new FolderBrowserDialog())
+            {
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    path = dialog.SelectedPath + "\\picture.png";// придумать нумерование
+                    textBox1.Text = path;
+                }
+            }
+        }
+    }
+}
